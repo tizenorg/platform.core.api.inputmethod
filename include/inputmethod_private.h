66eae444df59a7c0b0c246a45f359f922568414b
Jihoon Kim <jihoon48.kim@samsung.com>
2016-03-29 11:13:30 +0900

    Fix coding style
    Change-Id: Ibad7ebe783885c9ed6e5e8da314b6933e07c02d7

--- include/inputmethod_private.h
+++ include/inputmethod_private.h
@@ -17,29 +17,27 @@
 #ifndef __TIZEN_UIX_INPUTMETHOD_PRIVATE_H__
 #define __TIZEN_UIX_INPUTMETHOD_PRIVATE_H__
 
-struct _ime_context
-{
-    Ecore_IMF_Input_Panel_Layout layout;    /**< Input panel (keyboard) layout types */
-    int layout_variation;                   /**< Layout variation */
-    int cursor_pos;                         /**< Cursor position in edit field */
-    Ecore_IMF_Autocapital_Type autocapital_type;    /**< Auto capital mode */
-    Ecore_IMF_Input_Panel_Return_Key_Type return_key_type;  /**< "Return" key types */
-    Eina_Bool return_key_disabled;          /**< The state of "Return" key */
-    Eina_Bool prediction_allow;             /**< Boolean to allow predictive text */
-    Eina_Bool password_mode;                /**< Password input mode of edit field */
-    int imdata_size;                        /**< The size of application specific data to input panel */
-    Ecore_IMF_Input_Hints input_hint;       /**< Edit field hint */
-    Ecore_IMF_BiDi_Direction bidi_direction;/**< Bidirectional mode */
-    Ecore_IMF_Input_Panel_Lang language;    /**< Preferred input language */
-    unsigned int client_window;             /**< Client application window object */
+struct _ime_context {
+	Ecore_IMF_Input_Panel_Layout layout;    /**< Input panel (keyboard) layout types */
+	int layout_variation;                   /**< Layout variation */
+	int cursor_pos;                         /**< Cursor position in edit field */
+	Ecore_IMF_Autocapital_Type autocapital_type;    /**< Auto capital mode */
+	Ecore_IMF_Input_Panel_Return_Key_Type return_key_type;  /**< "Return" key types */
+	Eina_Bool return_key_disabled;          /**< The state of "Return" key */
+	Eina_Bool prediction_allow;             /**< Boolean to allow predictive text */
+	Eina_Bool password_mode;                /**< Password input mode of edit field */
+	int imdata_size;                        /**< The size of application specific data to input panel */
+	Ecore_IMF_Input_Hints input_hint;       /**< Edit field hint */
+	Ecore_IMF_BiDi_Direction bidi_direction;/**< Bidirectional mode */
+	Ecore_IMF_Input_Panel_Lang language;    /**< Preferred input language */
+	unsigned int client_window;             /**< Client application window object */
 };
 
-struct _ime_device_info
-{
-    const char *dev_name;                   /**< The device name */
-    Ecore_IMF_Device_Class dev_class;       /**< The device class */
-    Ecore_IMF_Device_Subclass dev_subclass; /**< The device subclass */
+struct _ime_device_info {
+	const char *dev_name;                   /**< The device name */
+	Ecore_IMF_Device_Class dev_class;       /**< The device class */
+	Ecore_IMF_Device_Subclass dev_subclass; /**< The device subclass */
 };
 
-#endif // __TIZEN_UIX_INPUTMETHOD_PRIVATE_H__
+#endif /* __TIZEN_UIX_INPUTMETHOD_PRIVATE_H__ */
 
